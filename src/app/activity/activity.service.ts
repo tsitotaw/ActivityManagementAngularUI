09c081b4db1c9bb1c3117517ade61ffadc4df702
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-03-13 20:23:26 -0500

    Fix angular issues

---
+++ src/app/activity/activity.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ConstantHelperService } from '../common/constant.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+
+  constructor(private httpClient:HttpClient, private constantHelperService:ConstantHelperService) { }
+
+  getAllActivities(){
+    return this.httpClient.get(this.constantHelperService.SERVER_API_URL + "activities");
+  }
+
+  getActivityById(typeId: string){
+    return this.httpClient.get(this.constantHelperService.SERVER_API_URL + "activities/"+ typeId);
+  }
+
+  updateActivityType(typeId:string, typeDetail:[string,string]){
+
+  }
+}
